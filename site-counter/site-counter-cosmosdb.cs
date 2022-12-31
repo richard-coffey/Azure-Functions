a8545d2e8e1a297599c736c2cc2c9709a039f18a
Richard Coffey <contact@richardcoffey.com>
2022-12-31 15:03:49 +0000

    refactor

--- site-counter/site-counter-cosmosdb.cs
+++ site-counter/site-counter-cosmosdb.cs
@@ -3,42 +3,26 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Cosmos;
 using System.Threading.Tasks;
 using Microsoft.Azure.Storage.Blob;
-using Microsoft.Extensions.Configuration;
 
 namespace SiteCounter
 {
     public class SiteCounter
     {
         public int Counter { get; set; }
-        public string id { get; set; } = "1";
+        public string Id { get; set; } = "1";
     }
 
     public static class SiteCounterCosmosDbFunction
     {
         [FunctionName("SiteCounterCosmosDbFunction")]
-        public static async Task Run([BlobTrigger("site-counter/{name}", Connection = "BlobContainerConnectionString")] CloudBlockBlob siteCounterBlob, string name, ILogger log, ExecutionContext context)
+        public static async Task Run([BlobTrigger("site-counter/{name}", Connection = "BlobContainerConnectionString")] CloudBlockBlob siteCounterBlob, string name, ILogger log,
+            [CosmosDB(
+                databaseName: "AzureServerlessCV",
+                collectionName: "SiteCounter",
+                ConnectionStringSetting = "DatabaseConnectionString"
+            )] IAsyncCollector<SiteCounter> siteCounterCollector)
         {
             log.LogInformation("SiteCounterCosmosDbFunction function processed a request.");
-
-            // Build a configuration object from the app settings
-            var config = new ConfigurationBuilder()
-                .SetBasePath(context.FunctionAppDirectory)
-                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables()
-                .Build();
-
-            // Get the storage blob connection string from the app settings
-            var storageBlobConnectionString = config.GetConnectionString("BlobContainerConnectionString");
-
-            // Get the CosmosDB connection string from the app settings
-            var cosmosDBConnectionString = config.GetConnectionString("DatabaseConnectionString");
-
-            // Connect to CosmosDB
-            CosmosClient cosmosClient = new CosmosClient(cosmosDBConnectionString);
-
-            // Get reference to database and container
-            var database = cosmosClient.GetDatabase("AzureServerlessCV");
-            var container = database.GetContainer("SiteCounter");
 
             // Read site counter value from blob
             string siteCounterValue = await siteCounterBlob.DownloadTextAsync();
@@ -48,7 +32,7 @@ namespace SiteCounter
             siteCounter.Counter = int.Parse(siteCounterValue);
 
             // Add site counter object to CosmosDB
-            await container.UpsertItemAsync<SiteCounter>(siteCounter, new PartitionKey(siteCounter.id));      
+            await siteCounterCollector.AddAsync(siteCounter);
         }
     }
 }
