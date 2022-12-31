d9f7e206f96f3cfd6345672468e4d1d87db56434
Richard Coffey <contact@richardcoffey.com>
2022-12-31 09:44:44 +0000

    new function

---
+++ site-counter/site-counter-cosmosdb-trigger.cs
@@ -0,0 +1,39 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Microsoft.Azure.Documents;
+
+namespace SiteCounter
+{
+    public static class CosmosDBCounterTriggerFunction
+    {
+        [FunctionName("CosmosDBCounterTriggerFunction")]
+        public static void Run([CosmosDBTrigger(
+            databaseName: "AzureServerlessCV",
+            collectionName: "SiteCounter",
+            ConnectionStringSetting = "DatabaseConnectionString",
+            LeaseCollectionName = "leases",
+            CreateLeaseCollectionIfNotExists = true
+        )]IReadOnlyList<Document> documents, ILogger log)
+        {
+            if (documents != null && documents.Count > 0)
+            {
+                var document = documents[0];
+                dynamic counterObject = JsonConvert.DeserializeObject<dynamic>(document.ToString());
+
+                // Check if the document is the counter with an id of "1"
+                if (counterObject.id == "1")
+                {
+                    // Store the counter value in a variable
+                    var counterValue = counterObject.counter;
+
+                    // Log the counter value
+                    log.LogInformation($"Counter value: {counterValue}");
+
+                    // You can use the counterValue variable to expose the value through an API
+                }
+            }
+        }
+    }
+}
