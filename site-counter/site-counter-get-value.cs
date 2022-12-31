cb19a2c0f827b6901f9835822adc8d8850d1587f
Richard Coffey <contact@richardcoffey.com>
2022-12-31 12:44:58 +0000

    CORS

--- site-counter/site-counter-get-value.cs
+++ site-counter/site-counter-get-value.cs
@@ -3,12 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Cors;
 
 namespace SiteCounter
 {
     public static class GetCounterValueFunction
     {
         [FunctionName("GetCounterValueFunction")]
+        [EnableCors("AllowAllOrigins")]
         public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]HttpRequest req, ILogger log)
         {
             // Read the value of the counter from the global variable
