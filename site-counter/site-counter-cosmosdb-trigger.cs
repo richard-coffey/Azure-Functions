5507abcca74f723d196fc231f4425b6109610066
Richard Coffey <contact@richardcoffey.com>
2022-12-31 18:08:15 +0000

    errors

--- site-counter/site-counter-cosmosdb-trigger.cs
+++ site-counter/site-counter-cosmosdb-trigger.cs
@@ -20,20 +20,29 @@ namespace SiteCounter
             CreateLeaseCollectionIfNotExists = true
         )]IReadOnlyList<Document> documents, ILogger log)
         {
-            if (documents != null && documents.Count > 0)
+            try
             {
-                var document = documents[0];
-                dynamic counterObject = JsonConvert.DeserializeObject<dynamic>(document.ToString());
-
-                // Check if the document is the counter with an id of "1"
-                if (counterObject.id == "1")
+                if (documents != null && documents.Count > 0)
                 {
-                    // Store the counter value in the global variable
-                    CounterValue = counterObject.Counter;
+                    var document = documents[0];
+                    dynamic counterObject = JsonConvert.DeserializeObject<dynamic>(document.ToString());
 
-                    // Log the counter value
-                    log.LogInformation($"Counter value: {CounterValue}");
+                    // Check if the document is the counter with an id of "1"
+                    if (counterObject.id == "1")
+                    {
+                        // Store the counter value in the global variable
+                        CounterValue = counterObject.Counter;
+
+                        // Log the counter value
+                        log.LogInformation($"Counter value: {CounterValue}");
+                    }
                 }
+            }
+            catch (System.Exception ex)
+            {
+                // If an error occurs, log the error and return the last good value
+                log.LogError(ex, "Error occurred while processing Cosmos DB trigger");
+                CounterValue = CounterValue > 0 ? CounterValue : 0;
             }
         }
     }
