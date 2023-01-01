732ff73fb84e28ae1ccc44276664151a4a92d544
Richard Coffey <contact@richardcoffey.com>
2023-01-01 12:29:17 +0000

    cosmosdb

--- site-counter/site-counter-get-value.cs
+++ site-counter/site-counter-get-value.cs
@@ -1,41 +1,37 @@
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
+using Microsoft.Azure.Cosmos;
+using System;
 
 namespace SiteCounter
 {
     public static class GetCounterValueFunction
     {
         [FunctionName("GetCounterValueFunction")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequestMessage req, ILogger log)
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequestMessage req,
+            ILogger log)
         {
-            log.LogInformation("GetCounterValueFunction function started");
+            // Read the Cosmos DB connection string and database name from app settings
+            string connectionString = Environment.GetEnvironmentVariable("DatabaseConnectionString");
 
-            // Create an HTTP client
-            HttpClient client = new HttpClient();
+            // Connect to Cosmos DB
+            CosmosClient cosmosClient = new CosmosClient(connectionString);
 
-            // Set the request URI and headers
-            string requestUri = "https://cosmosdb-azure-serverless-cv.documents.azure.com:443/dbs/AzureServerlessCV/colls/SiteCounter/docs/1";
-            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            // Get reference to database and container
+            var database = cosmosClient.GetDatabase("AzureServerlessCV");
+            var container = database.GetContainer("SiteCounter");
 
-            // Get an access token for the Cosmos DB resource using managed identity
-            var azureServiceTokenProvider = new AzureServiceTokenProvider();
-            string accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://cosmosdb.azure.com/");
+            // Read the site counter document
+            var siteCounterDoc = await container.ReadItemAsync<JObject>("1", new PartitionKey("1"));
+            var siteCounter = siteCounterDoc.Resource;
 
-            // Add the access token to the "authorization" header
-            client.DefaultRequestHeaders.Add("authorization", $"Bearer {accessToken}");
-
-            // Send the request and get the response
-            HttpResponseMessage response = await client.GetAsync(requestUri);
-            string responseContent = await response.Content.ReadAsStringAsync();
-
-            // Parse the response content and get the counter value
-            JObject responseObject = JObject.Parse(responseContent);
-            int counterValue = (int)responseObject["Counter"];
+            // Get the counter value
+            int counterValue = siteCounter["Counter"].Value<int>();
 
             // Return the counter value in the response body
             return req.CreateResponse(System.Net.HttpStatusCode.OK, counterValue);
