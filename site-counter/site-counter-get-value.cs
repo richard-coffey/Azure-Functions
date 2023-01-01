4587c010baabb42e69526edaf8eb1a7e63b36fc1
Richard Coffey <contact@richardcoffey.com>
2023-01-01 10:47:26 +0000

    revert

--- site-counter/site-counter-get-value.cs
+++ site-counter/site-counter-get-value.cs
@@ -8,16 +8,16 @@ namespace SiteCounter
 {
     public static class GetCounterValueFunction
     {
-        private static int? lastGoodCounterValue = null;
+        private static int lastGoodCounterValue = 0;
 
         [FunctionName("GetCounterValueFunction")]
         public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]HttpRequest req, ILogger log)
         {
             // Read the value of the counter from the global variable
-            int? counterValue = CosmosDBCounterTriggerFunction.CounterValue;
+            int counterValue = CosmosDBCounterTriggerFunction.CounterValue;
 
-            // If the counter value is null, return the last good value instead
-            if (counterValue == null)
+            // If the counter value is 0, return the last good value instead
+            if (counterValue == 0)
             {
                 return new OkObjectResult(lastGoodCounterValue);
             }
